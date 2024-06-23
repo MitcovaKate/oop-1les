c0834fe07b4bd31eb545c2a326e17efcfb64b44c
MitcovaKate <katya.mitkova@bk.ru>
2024-06-23 22:17:00 +0300

    food

---
+++ food/index.php
@@ -0,0 +1,17 @@
+<?
+class Food{
+    public $name;
+    public $image;
+    public $price;
+
+public function __constructor($name , $image , $price){
+  $this ->name=$name;
+  $this ->image=$image;
+  $this->price=$price;
+}
+
+
+}
+
+
+?>
