89827232d5705de8489e82cfe969f57c0f002897
MitcovaKate <katya.mitkova@bk.ru>
2024-07-06 22:58:08 +0300

    currency limit/int limit

--- oop2/src/Money.php
+++ oop2/src/Money.php
@@ -2,7 +2,6 @@
 class Money{
     private int $amount;
     private string $currency;
-
     public function __construct(int $amount, string $currency)
     {
         $this->setAmount($amount);
@@ -10,13 +9,24 @@ class Money{
         
     }
     public function setAmount(int $amount):void{
-      $this->amount=$amount;
+      $this->amount = min(max($amount, PHP_INT_MIN), PHP_INT_MAX);#HW4
     }
     public function getAmount(int $amount):int{
      return   $this->amount;
       }
-      public function setCurrency(int $currency):void{
-        $this->currency=$currency;
+      public function setCurrency(int $currency): void#HW5
+      {
+          $currencyLIMIT = [
+              'EUR' => 1,
+              'MDL' => 2,
+              'USD' => 3,
+          ];
+      
+          if (!isset($currencyLIMIT[$currency])) {
+              die('Invalid currency. Allowed currencies are: EUR, MDL, USD');
+          }
+      
+          $this->currency = $currency;
       }
       public function getCurrency(int $currency):int{
        return   $this->currency;
