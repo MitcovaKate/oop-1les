cc78332b0c1bc41722ca3c1e17855be94693aa3c
MitcovaKate <katya.mitkova@bk.ru>
2024-08-03 20:48:11 +0300

    interface

---
+++ oop2/src/SwitchebleInterface.php
@@ -0,0 +1,6 @@
+<?
+interface SwitchbleInterface{
+    public function turnOn():void;
+    public function turnOff():void;
+    public function isOn():bool;
+}
--- oop2/src/Tel.php
+++ oop2/src/Tel.php
@@ -1,6 +1,7 @@
 <?
 require_once './src/Product.php';
 require_once './src/SwitchingTrait.php';
+require_once './SwitchebleInterface.php';
 class Tel extends Product{
     public int $diagonal;
     use SwitchingTrait;
@@ -11,5 +12,26 @@ class Tel extends Product{
         $this->isOn=false;
         
     }
+    //interface logic
+    //class Tel extends Product implements SwitchbleInterface{
+       // public int $diagonal;
+      // private bool $status;
+      //  public function __construct(string $name, int $price ,int $diagonal)
+      //  {
+      //parent::__construct($name, $price);
+         //   $this->diagonal=$diagonal;
+         //   $this->status =false;
+            // }
+           // public function turnOn():void{
+          //$this->status=true; 
+           // }
+            // public function turnOff():void{
+          //$this->status=false; 
+           // }
+            // public function isOn():bool{
+          //return $this -> status; 
+           // }
+           
+   // }
     
 }
